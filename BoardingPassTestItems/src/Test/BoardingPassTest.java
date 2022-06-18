f7783728ba4a7dc4722a810b3dcd8b10a7296dd1
Niko <nikovk25@gmail.com>
2022-06-18 17:33:51 -0400

    BoardingPassTestItems

---
+++ BoardingPassTestItems/src/Test/BoardingPassTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BoardingPassTest {
+
+    BoardingPass boardingpass;
+
+    @BeforeEach
+    void setUp() {
+        boardingpass = new BoardingPass();
+    }
+
+    @Test
+    void main() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+}
---
+++ BoardingPassTestItems/src/Test/UserInfoTest.java
@@ -0,0 +1,21 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserInfoTest {
+    UserInfo userinfo;
+
+    @BeforeEach
+    void setUp() {
+        userinfo = new UserInfo();
+    }
+
+    @Test
+    void getListeners() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
